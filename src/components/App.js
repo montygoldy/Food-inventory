e74a2fd96aa295762d3835fe02b4e1da19258bdc
montygoldy <montygoldy2006@gmail.com>
2018-01-14 19:03:12 -0500

    Created components and scss files for order and inventory

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
 class App extends React.Component{
   render(){
---
+++ src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/inventory.scss';
+
+class Inventory extends React.Component{
+  render(){
+    return (
+      <p>Inventory</p>
+    )
+  }
+}
+
+export default Inventory;
