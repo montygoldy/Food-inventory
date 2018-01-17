47d7f85be49771a02d24205ddace8f0218ea7f9b
montygoldy <montygoldy2006@gmail.com>
2018-01-17 18:01:25 -0500

    Created a custom funtion for handling submit and added transitionTo of the router with the use of context

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import '../css/storePicker.scss';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+
+  constructor(){
+    super()
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(e){
+    e.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a Store</h2>
-        <input type="text" placeholder="Store Name" required/>
+        <input type="text" ref={(input) => {this.storeInput = input}} placeholder="Store Name" defaultValue={getFunName()}  required/>
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
